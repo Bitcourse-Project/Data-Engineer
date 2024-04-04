c69b17fabf16baf986393d5243d18856b05ebad0
seongcheollee <jhk6520736@gmail.com>
2024-04-04 21:11:24 +0900

    Edit db condition

--- jobs/python/catchDiversions.py
+++ jobs/python/catchDiversions.py
@@ -6,6 +6,10 @@ from pyspark.sql.types import *
 from decimal import Decimal
 import pyspark.sql.types as T
 from pyspark.sql import functions as F
+from kafka import KafkaProducer
+import config
+import sys
+from kafka import KafkaProducer
 
 
 def up_diversions(df):
@@ -40,35 +44,65 @@ def down_diversions(df):
 
 def set_diver_state(df, n):
     df = df.select("datetime","symbol","close").limit(1)
-    df = df.withColumn("diversions", F.lit(1))
+    df = df.withColumn("diversions", F.lit(n))
+    df = df.withColumn("convert", F.lit(0))
+    db = "coin"
     # producer send
     df.write.format("jdbc") \
-    .option("url", "jdbc:postgresql://postgres-coin:5432/coin") \
+    .option("url","jdbc:postgresql://{0}:{1}/{2}".format(config.psql_ip, config.psql_port, db ) )\
     .option("driver", "org.postgresql.Driver") \
-    .option("user", user) \
-    .option("password", passwd) \
+    .option("user", config.psql_user) \
+    .option("password", config.psql_passwd) \
     .option("dbtable", "now_diversions") \
     .mode("overwrite") \
     .save()
 
+    return df
+
+def get_state():
+    db = "coin"
+    sql = "select * from task_state"
+    df = spark.read.format("jdbc")\
+                    .option("url","jdbc:postgresql://{0}:{1}/{2}".format(config.psql_ip, config.psql_port, db ) )\
+                    .option("driver", "org.postgresql.Driver")\
+                    .option("user", config.psql_user) \
+                    .option("password", config.psql_passwd) \
+                    .option("query", sql)\
+                    .load()
+    df.show()
+    state = df.first()["state"]
+    return state
+
 def set_state(n):
-    
     data = [(n,)]
     columns = ["state"]
     task = spark.createDataFrame(data, columns)
-
+    db = "coin"
     task.write.format("jdbc") \
-    .option("url", "jdbc:postgresql://postgres-coin:5432/coin") \
+    .option("url","jdbc:postgresql://{0}:{1}/{2}".format(config.psql_ip, config.psql_port, db ) )\
     .option("driver", "org.postgresql.Driver") \
-    .option("user", user) \
-    .option("password", passwd) \
+    .option("user", config.psql_user) \
+    .option("password", config.psql_passwd) \
     .option("dbtable", "task_state") \
     .mode("overwrite") \
     .save()
 
+def send_message(df):
+    # 수정필요
+    json_strings = df.toJSON().collect()
+    #producer.send(topic, row.encode('utf-8'))
+
 spark = SparkSession.builder.appName("binance")\
     .config("spark.driver.extraClassPath", "/opt/bitnami/spark/jars/postgresql-42.7.1.jar") \
     .getOrCreate()
+
+if get_state():
+    print("다이버전스가 이미 떠 있습니다.")
+    sys.exit()
+
+# topic = 'coin'
+# kafka_bootstrap_servers = 'kafka-div:9092'
+# producer = KafkaProducer(bootstrap_servers=[kafka_bootstrap_servers])
 
 
 ip = "postgres-coin"
@@ -79,22 +113,23 @@ passwd = "postgres"
 sql = "SELECT * FROM kline_1d_rsi ORDER BY datetime DESC LIMIT 30"
 
 kline_1d_rsi = spark.read.format("jdbc")\
-                    .option("url","jdbc:postgresql://{0}:{1}/{2}".format( ip, port, db ) )\
+                    .option("url","jdbc:postgresql://{0}:{1}/{2}".format(config.psql_ip, config.psql_port, db ) )\
                     .option("driver", "org.postgresql.Driver")\
-                    .option("user", user)\
+                    .option("user", config.psql_user) \
+                    .option("password", config.psql_passwd) \
                     .option("query", sql)\
-                    .option("password", passwd)\
                     .load()
-
 
 
 if up_diversions(kline_1d_rsi):
     # kafka send 추가
-    set_diver_state(kline_1d_rsi, 1)
+    df = set_diver_state(kline_1d_rsi, 1)
     set_state(1)
+    #send_message(df)
     
-
 if down_diversions(kline_1d_rsi):
     # kafka send
-    set_diver_state(kline_1d_rsi, 2)
-    set_state(2)
+    df = set_diver_state(kline_1d_rsi, 2)
+    set_state(1)
+    #send_message(df)
+
